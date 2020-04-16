9c1491bcd785c6014440a5482393812e7529f838
Juan Pablo Botero <juan.botero@ceiba.com.co>
2020-04-16 08:03:09 -0500

    otro

---
+++ web-build/precache-manifest.b40f4f037f01d767650b3c055bdbd500.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8cd563acad45f432f92",
+    "url": "/static/js/app.579a17c9.chunk.js"
+  },
+  {
+    "revision": "328ec0abaf4b8f214d7f",
+    "url": "/static/js/runtime~app.b3244af7.js"
+  },
+  {
+    "revision": "3ae54675844fe4cf2aa5",
+    "url": "/static/js/2.cb53ac41.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "6e89aa5ecab02e18ca7de98a919cfb3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "271fcdc2834ebbc4ec1c1c78fd63f7ce",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
